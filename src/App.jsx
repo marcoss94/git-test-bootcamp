f303aefc8385cd7ce45c38134e372f13c52c565f
Marcos <marcos.alvarez@tolber.io>
2023-11-30 19:28:16 -0300

    add text in main

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ function App() {
   return (
     <>
       <nav>Navbar</nav>
-
       <p className="read-the-docs">Click on the</p>
-
       <span>Certificado GPTW</span>
-
+      esto se creo en la rama main
       <section>
         lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem
         ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem ipsum
